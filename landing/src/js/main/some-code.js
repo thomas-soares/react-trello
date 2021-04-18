acda930afb2722d6bfc3b24226758c7d4771dabc
thomas-soares <thomas__soares@hotmail.com>
2021-04-18 08:09:00 -0300

    Javascript pra suavizar scroll ao ancorar página.

--- landing/src/js/main/some-code.js
+++ landing/src/js/main/some-code.js
@@ -4,3 +4,18 @@ document.addEventListener('click', function (event) {
 }, false);
 
 console.log("FOFOFIFOFIFOFIF")
+
+$(document).ready(function() {
+	$(document).on('click', 'a[href^="#"]', function(e) {
+		var id = $(this).attr('href');
+		var target = $(id);
+
+		if (target.length === 0) {
+			return;
+		}
+
+		e.preventDefault();
+
+		$('body, html').animate({ scrollTop: target.offset().top });
+	});
+});
