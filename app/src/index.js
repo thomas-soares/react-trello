f4df9422ff7b21d0fddfe20049cac7ae1bd93b2f
Thomas Soares <thomas__soares@hotmail.com>
2021-04-21 23:16:28 -0300

    Index modificada pra usar o Redux.

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router';
+import { ConnectedRouter } from 'react-router-redux';
+
+import Home from './containers/Home';
+import cfg from './store';
+
+const store = cfg.configStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <main>
+    <Provider store={ store }>
+      <ConnectedRouter history = { cfg.history }>
+        <Route exact path="/" component={ Home }></Route>
+      </ConnectedRouter>
+    </Provider>
+  </main>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
